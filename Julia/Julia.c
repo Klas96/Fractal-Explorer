5544e3235b891f0e7911477d8ac5d933cf1965e1
Klas Holmgren <klas0holmgren@gmail.com>
2022-01-31 20:13:40 +0100

    Argument parser function

--- Julia/Julia.c
+++ Julia/Julia.c
@@ -6,6 +6,50 @@
 #include "../getColor.h"
 
 
+double* parseArguments(int argc, char* argv[]) {
+
+  double* ret = malloc(4*sizeof(double));
+
+  double xmin = -2;
+  double xmax = 2;
+  double ymin = -2;
+  double ymax = 2;
+
+  if(argc<=4) {
+    printf("Not enugh Arguments Entered using default area. \n ");
+    printf("x: %f to %f and y: %f to %f \n", xmin, xmax, ymin, ymax);
+  }
+  else {
+    if (atof(argv[1]) < atof(argv[2])) {
+      xmin = atof(argv[1]);
+      xmax = atof(argv[2]);
+    }
+    else {
+      printf("xMin: %f have to be smaler than xMax= %f", atof(argv[1]), atof(argv[2]));
+    }
+
+    if (atof(argv[3]) < atof(argv[4])) {
+      ymin = atof(argv[3]);
+      ymax = atof(argv[4]);
+    }
+    else {
+      printf("yMin: %f have to be smaler than yMax= %f", atof(argv[3]), atof(argv[4]));
+    }
+
+    printf("x: %f to %f and y: %f to %f \n", xmin, xmax, ymin, ymax);
+    // TODO Check if arguments is valid
+    // That is min < max
+  }
+
+  ret[0] = xmin;
+  ret[1] = xmax;
+  ret[2] = ymin;
+  ret[3] = ymax;
+
+  return(ret);
+}
+
+
 int main(int argc, char* argv[]){
 
   /* The window in the plane. */
@@ -26,7 +70,7 @@ int main(int argc, char* argv[]){
       xmax = atof(argv[2]);
     }
     else {
-      printf("xMin: %f have to be smaler than xMax= %f", atof(argv[1], atof(argv[2]);
+      printf("xMin: %f have to be smaler than xMax= %f", atof(argv[1]), atof(argv[2]));
     }
 
     if (atof(argv[3]) < atof(argv[4])) {
@@ -34,9 +78,9 @@ int main(int argc, char* argv[]){
       ymax = atof(argv[4]);
     }
     else {
-      printf("yMin: %f have to be smaler than yMax= %f", atof(argv[3], atof(argv[4]);
+      printf("yMin: %f have to be smaler than yMax= %f", atof(argv[3]), atof(argv[4]));
     }
-    
+
     printf("x: %f to %f and y: %f to %f \n", xmin, xmax, ymin, ymax);
     // TODO Check if arguments is valid
     // That is min < max
