ef74efe64dbc597b1c85a0ef770da6a1d60f4a99
Klas Holmgren <klas0holmgren@gmail.com>
2022-01-31 20:25:08 +0100

    Argument Parser

--- Julia/Julia.c
+++ Julia/Julia.c
@@ -6,9 +6,7 @@
 #include "../getColor.h"
 
 
-double* parseArguments(int argc, char* argv[]) {
-
-  double* ret = malloc(4*sizeof(double));
+double* parseArguments(int argc, char* argv[], double* ret) {
 
   double xmin = -2;
   double xmax = 2;
@@ -52,39 +50,13 @@ double* parseArguments(int argc, char* argv[]) {
 
 int main(int argc, char* argv[]){
 
-  /* The window in the plane. */
-  double xmin = -2;
-  double xmax = 2;
-  double ymin = -2;
-  double ymax = 2;
+  double arguments[4];
+  parseArguments(argc, argv, arguments);
 
-  /* The window in the plane. */
-  if(argc<=4) {
-      printf("Not enugh Arguments Entered using default area. \n ");
-
-      printf("x: %f to %f and y: %f to %f \n", xmin, xmax, ymin, ymax);
-  }
-  else {
-    if (atof(argv[1]) < atof(argv[2])) {
-      xmin = atof(argv[1]);
-      xmax = atof(argv[2]);
-    }
-    else {
-      printf("xMin: %f have to be smaler than xMax= %f", atof(argv[1]), atof(argv[2]));
-    }
-
-    if (atof(argv[3]) < atof(argv[4])) {
-      ymin = atof(argv[3]);
-      ymax = atof(argv[4]);
-    }
-    else {
-      printf("yMin: %f have to be smaler than yMax= %f", atof(argv[3]), atof(argv[4]));
-    }
-
-    printf("x: %f to %f and y: %f to %f \n", xmin, xmax, ymin, ymax);
-    // TODO Check if arguments is valid
-    // That is min < max
-  }
+  double xmin = arguments[0];
+  double xmax = arguments[1];
+  double ymin = arguments[2];
+  double ymax = arguments[3];
 
   /* Maximum number of iterations, at most 65535. */
   //Seams to change the color
